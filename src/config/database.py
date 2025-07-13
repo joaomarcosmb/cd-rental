db52e9ee37ea19728c872a32bf0e9006704bf604
Joao Barroso <joao.barroso@alu.ufc.br>
2025-07-12 23:08:27 -0300

    chore: update docker-compose configuration, add healthcheck for PostgreSQL, and include psycopg2-binary in requirements

--- src/config/database.py
+++ src/config/database.py
@@ -5,10 +5,10 @@ from flask_migrate import Migrate
 db = SQLAlchemy()
 migrate = Migrate()
 
-# config postgres db
 def config_db(app):
     app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost:5432/rental_db'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SQLALCHEMY_ECHO'] = True
 
     db.init_app(app)
     migrate.init_app(app, db)
